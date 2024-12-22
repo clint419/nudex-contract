fe612b9ca8c3dc988022a0a34a542c63994a0cab
Li-Qing Wang <liqingnz@gmail.com>
2024-12-22 16:18:51 +0800

    feat: add mock data script for funds and asset, update deployed contract addresses

--- README.md
+++ README.md
@@ -6,12 +6,8 @@ Smart contracts used by nuDex deposits and withdraw
 
 > BSC Testnet
 
-| Contract           | Address                                    |
-| ------------------ | ------------------------------------------ |
-| NuvoToken          | 0xF02B7f15f0F9826F1625F9C458C04DE6592557A1 |
-| NuvoLock           | 0x36451DD16C926cC6cCf54F127aF091db2072FCB1 |
-| TaskManager        | 0x557D35D136273e61EE1A7252d422F5a9C74b58d9 |
-| ParticipantHandler | 0x5B91122E30b222E4619E0975C03aAE5eDD8fCC21 |
-| AccountHandler     | 0x5d4C4c4aF9DD2E4fA5E4424ADcdC4581ebE39c9E |
-| AssetHandler       | 0xA18bFd95400bFE1e7F9D456Ec14AFF79d16E81A3 |
-| FundsHandler       | 0x59C66Bb82B873420D484040Fb69a830BD39d229b |
+| Contract                | Address                                    |
+| ----------------------- | ------------------------------------------ |
+| AccountHandler          | 0x4c136D2c6aC3D3c6F782082124fc6D2315302077 |
+| AssetHandlerUpgradeable | 0xfB25A9E65b01593aB5b232D3b744588c7c39Eba5 |
+| FundsHandlerUpgradeable | 0x25C3BF79Fd6B3e55502ad59Ab67e3C449E2293fD |
--- script/MockData.sol
+++ script/MockData.sol
@@ -6,23 +6,124 @@ import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 import {NuvoProxy, ITransparentUpgradeableProxy} from "../src/proxies/NuvoProxy.sol";
 import {AccountHandlerUpgradeable} from "../src/handlers/AccountHandlerUpgradeable.sol";
+import {AssetHandlerUpgradeable, AssetParam, TokenInfo} from "../src/handlers/AssetHandlerUpgradeable.sol";
+import {FundsHandlerUpgradeable} from "../src/handlers/FundsHandlerUpgradeable.sol";
+import {TaskManagerUpgradeable} from "../src/TaskManagerUpgradeable.sol";
 import {IAccountHandler} from "../src/interfaces/IAccountHandler.sol";
 
 // this contract is only used for contract testing
 contract MockData is Script {
-    function run() public {
-        uint256 deployerPrivateKey = vm.envUint("PARTICIPANT_KEY_1");
-        address deployer = vm.createWallet(deployerPrivateKey).addr;
-        console.log("Deployer address: ", deployer);
+    bytes32 public constant TICKER = "TOKEN_TICKER_18";
+    bytes32 public constant CHAIN_ID = 0;
+    bytes32 public constant FUNDS_ROLE = keccak256("FUNDS_ROLE");
+
+    uint256 public deployerPrivateKey;
+    address public deployer;
+
+    AccountHandlerUpgradeable accountManager;
+    AssetHandlerUpgradeable assetHandler;
+    FundsHandlerUpgradeable fundsHandler;
+
+    address[] public handlers;
+
+    function setUp() public {
+        deployerPrivateKey = vm.envUint("PARTICIPANT_KEY_1");
+        deployer = vm.createWallet(deployerPrivateKey).addr;
+
         vm.startBroadcast(deployerPrivateKey);
 
+        TaskManagerUpgradeable taskManager = new TaskManagerUpgradeable();
+
         // deploy accountManager
-        AccountHandlerUpgradeable accountManager = new AccountHandlerUpgradeable(address(0));
+        accountManager = new AccountHandlerUpgradeable(address(taskManager));
         NuvoProxy proxy = new NuvoProxy(address(accountManager), vm.envAddress("PARTICIPANT_2"));
         accountManager = AccountHandlerUpgradeable(address(proxy));
         accountManager.initialize(deployer, deployer, deployer);
+        handlers.push(address(accountManager));
         console.log("|AccountHandler|", address(accountManager));
 
+        // deploy assetHandler
+        assetHandler = new AssetHandlerUpgradeable(address(taskManager));
+        proxy = new NuvoProxy(address(assetHandler), vm.envAddress("PARTICIPANT_2"));
+        assetHandler = AssetHandlerUpgradeable(address(proxy));
+        assetHandler.initialize(deployer, deployer, deployer);
+        handlers.push(address(assetHandler));
+        console.log("|AssetHandlerUpgradeable|", address(assetHandler));
+
+        // deploy fundsHandler
+        fundsHandler = new FundsHandlerUpgradeable(address(assetHandler), address(taskManager));
+        proxy = new NuvoProxy(address(fundsHandler), vm.envAddress("PARTICIPANT_2"));
+        fundsHandler = FundsHandlerUpgradeable(address(proxy));
+        fundsHandler.initialize(deployer, deployer, deployer);
+        handlers.push(address(fundsHandler));
+        assetHandler.grantRole(FUNDS_ROLE, address(fundsHandler));
+        console.log("|FundsHandlerUpgradeable|", address(fundsHandler));
+
+        taskManager.initialize(deployer, deployer, handlers);
+        console.log("Deployer address: ", deployer);
+
+        vm.stopBroadcast();
+    }
+
+    function run() public {
+        vm.startBroadcast(deployerPrivateKey);
+        accountData();
+        fundsData();
+        vm.stopBroadcast();
+    }
+
+    function fundsData() public {
+        // asset
+        AssetParam memory assetParam = AssetParam(18, true, true, 1 ether, 1 ether, "Token_Alias");
+        assetHandler.submitListAssetTask(TICKER, assetParam);
+        assetHandler.listNewAsset(TICKER, assetParam);
+        TokenInfo[] memory testTokenInfo = new TokenInfo[](1);
+        testTokenInfo[0] = TokenInfo(
+            CHAIN_ID,
+            true,
+            uint8(18),
+            "0xContractAddress",
+            "SYMBOL",
+            0,
+            100 ether
+        );
+        assetHandler.submitAssetTask(
+            TICKER,
+            abi.encodeWithSelector(assetHandler.linkToken.selector, TICKER, testTokenInfo)
+        );
+        assetHandler.linkToken(TICKER, testTokenInfo);
+
+        // deposit/withdraw
+        fundsHandler.submitDepositTask(
+            deployer,
+            "124wd5urvxo4H3naXR6QACP1MGVpLeikeR",
+            TICKER,
+            CHAIN_ID,
+            1 ether
+        );
+        fundsHandler.recordDeposit(
+            deployer,
+            "124wd5urvxo4H3naXR6QACP1MGVpLeikeR",
+            TICKER,
+            CHAIN_ID,
+            1 ether
+        );
+        fundsHandler.submitWithdrawTask(
+            deployer,
+            "124wd5urvxo4H3naXR6QACP1MGVpLeikeR",
+            TICKER,
+            CHAIN_ID,
+            1 ether
+        );
+        fundsHandler.recordWithdrawal(
+            "124wd5urvxo4H3naXR6QACP1MGVpLeikeR",
+            TICKER,
+            CHAIN_ID,
+            1 ether
+        );
+    }
+
+    function accountData() public {
         for (uint8 i; i < 10; ++i) {
             accountManager.registerNewAddress(
                 deployer,
@@ -76,7 +177,5 @@ contract MockData is Script {
             2,
             "8ymc6niJiF4imco29UU3z7mK11sCt9NdL3LjG3VkEYAC"
         );
-
-        vm.stopBroadcast();
     }
 }
