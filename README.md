55a131ce69d86dc814912fee2827bdaebad6249e
Li-Qing Wang <liqingnz@gmail.com>
2025-01-19 20:07:56 +0800

    chore: add deployed contract addresses

--- README.md
+++ README.md
@@ -21,3 +21,21 @@ submitter: 0x1D2cd50A3cF3c55a7982AD54F9f364C1e953Bc57
 participant 0 address: 0x3a818294ca1F3C27d7588b123Ec43F2546fa07f4
 participant 1 address: 0x04d9389Cf937b1e6F2258d842e7237E955d6ab04
 participant 2 address: 0xf6D37CE75dB465DcDb4c7097bEB9c1D46b171037
+
+> BSC Testnet
+
+| Contract           | Address                                    |
+| ------------------ | ------------------------------------------ |
+| EntryPoint         | 0x6a3E7DE1B9eFa5aEe328E4B40d8Adc8E36D363AA |
+| NuvoToken          | 0x59a955D74ef2137D9687bcdd076494789bda6c8F |
+| NuvoLock           | 0x9e8D581303D3c8EaE84AFF156650B062A89ef9EF |
+| TaskManager        | 0x910973f2d9E4d5eDe0C8F0E58Ce707b25F908128 |
+| ParticipantHandler | 0xB0CA87f571448f707aC184C699FA2493bFfD06cc |
+| AccountHandler     | 0x28D2451fB93D3E8051522f455d64b4cEa80F7254 |
+| AssetHandler       | 0x12cDb526df03A045894836fD75d612B08F50F428 |
+| FundsHandler       | 0x176D5a6DC9237A6e6f7ba94379c552E9BCB230f2 |
+
+submitter: 0x1D2cd50A3cF3c55a7982AD54F9f364C1e953Bc57
+participant 0 address: 0x3a818294ca1F3C27d7588b123Ec43F2546fa07f4
+participant 1 address: 0x04d9389Cf937b1e6F2258d842e7237E955d6ab04
+participant 2 address: 0xf6D37CE75dB465DcDb4c7097bEB9c1D46b171037
--- script/SignatureGenerator.sol
+++ script/SignatureGenerator.sol
@@ -6,21 +6,27 @@ import {Script, console} from "forge-std/Script.sol";
 import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-import {EntryPointUpgradeable} from "../src/EntryPointUpgradeable.sol";
+import {EntryPointUpgradeable, TaskOperation} from "../src/EntryPointUpgradeable.sol";
 
 contract SignatureGenerator is Script {
     using MessageHashUtils for bytes32;
     using ECDSA for bytes32;
 
-    EntryPointUpgradeable votingManager;
+    EntryPointUpgradeable entryPoint;
 
     function setUp() public {
-        votingManager = EntryPointUpgradeable(vm.envAddress("VOTING_MANAGER_ADDR"));
+        entryPoint = EntryPointUpgradeable(vm.envAddress("VOTING_MANAGER_ADDR"));
+    }
+
+    function run() public {
+        uint256[] memory temp = new uint256[](3);
+        temp[0] = 22;
+        // generateOptSignature(temp);
     }
 
     // forge script --rpc-url localhost script/SignatureGenerator.sol --sig "run((address,uint8, uint64, bytes)[],uint256)"
     function run(uint64[] calldata _taskIds, uint256 _privKey) public view returns (bytes memory) {
-        bytes memory encodedData = abi.encode(_taskIds, votingManager.tssNonce(), block.chainid);
+        bytes memory encodedData = abi.encode(_taskIds, entryPoint.tssNonce(), block.chainid);
         bytes32 digest = keccak256(encodedData).toEthSignedMessageHash();
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(_privKey, digest);
         console.logBytes(abi.encodePacked(r, s, v));
@@ -35,5 +41,18 @@ contract SignatureGenerator is Script {
     // forge script script/SignatureGenerator.sol --sig "recover(bytes32,bytes)"
     function recover(bytes32 digest, bytes calldata signature) public pure returns (address) {
         return digest.recover(signature);
+    }
+
+    // forge script --rpc-url bscTestnet script/SignatureGenerator.sol --sig "generateOptSignature((uint64,uint8,bytes)[], uint256, uint256, uint256)" "[(0, 2, 0x)]" 0 56 1234
+    function generateOptSignature(
+        TaskOperation[] memory _operations,
+        uint256 _nonce,
+        uint256 _chainId,
+        uint256 _privateKey
+    ) public view returns (bytes memory) {
+        bytes memory encodedData = abi.encode(_operations, _nonce, _chainId);
+        bytes32 digest = keccak256(encodedData).toEthSignedMessageHash();
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(_privateKey, digest);
+        return abi.encodePacked(r, s, v);
     }
 }
