11938afedc34d060805ea37df40b60229ebc2c69
Li-Qing Wang <liqingnz@gmail.com>
2025-01-28 19:52:25 +0800

    chore: separate MockData script into different modules

--- script/MockData.s.sol
+++ script/MockData.s.sol
@@ -28,52 +28,61 @@ contract MockData is Script {
     address[] public handlers;
 
     function setUp() public {
-        deployerPrivateKey = vm.envUint("PARTICIPANT_KEY_1");
+        deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         deployer = vm.createWallet(deployerPrivateKey).addr;
 
         vm.startBroadcast(deployerPrivateKey);
-
-        taskManager = new TaskManagerUpgradeable();
-        console.log("|TaskManager|", address(taskManager));
-
-        // deploy accountHandler
-        accountHandler = new AccountHandlerUpgradeable(address(taskManager));
-        // NuvoProxy proxy = new NuvoProxy(address(accountHandler), vm.envAddress("PARTICIPANT_2"));
-        // accountHandler = AccountHandlerUpgradeable(address(proxy));
-        accountHandler.initialize(deployer, deployer, deployer);
-        handlers.push(address(accountHandler));
-        console.log("|AccountHandler|", address(accountHandler));
-
-        // deploy assetHandler
-        assetHandler = new AssetHandlerUpgradeable(address(taskManager));
-        // proxy = new NuvoProxy(address(assetHandler), vm.envAddress("PARTICIPANT_2"));
-        // assetHandler = AssetHandlerUpgradeable(address(proxy));
-        assetHandler.initialize(deployer, deployer, deployer);
-        handlers.push(address(assetHandler));
-        console.log("|AssetHandlerUpgradeable|", address(assetHandler));
-
-        // deploy fundsHandler
-        fundsHandler = new FundsHandlerUpgradeable(address(assetHandler), address(taskManager));
-        // proxy = new NuvoProxy(address(fundsHandler), vm.envAddress("PARTICIPANT_2"));
-        // fundsHandler = FundsHandlerUpgradeable(address(proxy));
-        fundsHandler.initialize(deployer, deployer, deployer);
-        handlers.push(address(fundsHandler));
-        assetHandler.grantRole(FUNDS_ROLE, address(fundsHandler));
-        console.log("|FundsHandlerUpgradeable|", address(fundsHandler));
-
-        taskManager.initialize(deployer, deployer, handlers);
-        console.log("Deployer address: ", deployer);
-
+        _setupContracts(true);
         vm.stopBroadcast();
+    }
+
+    function _setupContracts(bool _fromEnv) internal {
+        if (_fromEnv) {
+            taskManager = TaskManagerUpgradeable(vm.envAddress("TASK_MANAGER"));
+            accountHandler = AccountHandlerUpgradeable(vm.envAddress("ACCOUNT_HANDLER"));
+            assetHandler = AssetHandlerUpgradeable(vm.envAddress("ASSET_HANDLER"));
+            fundsHandler = FundsHandlerUpgradeable(vm.envAddress("FUNDS_HANDLER"));
+        } else {
+            taskManager = new TaskManagerUpgradeable();
+            console.log("|TaskManager|", address(taskManager));
+
+            // deploy accountHandler
+            accountHandler = new AccountHandlerUpgradeable(address(taskManager));
+            // NuvoProxy proxy = new NuvoProxy(address(accountHandler), vm.envAddress("PARTICIPANT_2"));
+            // accountHandler = AccountHandlerUpgradeable(address(proxy));
+            accountHandler.initialize(deployer, deployer, deployer);
+            handlers.push(address(accountHandler));
+            console.log("|AccountHandler|", address(accountHandler));
+
+            // deploy assetHandler
+            assetHandler = new AssetHandlerUpgradeable(address(taskManager));
+            // proxy = new NuvoProxy(address(assetHandler), vm.envAddress("PARTICIPANT_2"));
+            // assetHandler = AssetHandlerUpgradeable(address(proxy));
+            assetHandler.initialize(deployer, deployer, deployer);
+            handlers.push(address(assetHandler));
+            console.log("|AssetHandlerUpgradeable|", address(assetHandler));
+
+            // deploy fundsHandler
+            fundsHandler = new FundsHandlerUpgradeable(address(assetHandler), address(taskManager));
+            // proxy = new NuvoProxy(address(fundsHandler), vm.envAddress("PARTICIPANT_2"));
+            // fundsHandler = FundsHandlerUpgradeable(address(proxy));
+            fundsHandler.initialize(deployer, deployer, deployer);
+            handlers.push(address(fundsHandler));
+            assetHandler.grantRole(FUNDS_ROLE, address(fundsHandler));
+            console.log("|FundsHandlerUpgradeable|", address(fundsHandler));
+
+            taskManager.initialize(deployer, deployer, handlers);
+            console.log("Deployer address: ", deployer);
+        }
     }
 
     function run() public {
         vm.startBroadcast(deployerPrivateKey);
 
-        assetData();
-        accountData();
-        fundsData();
-        updateTask();
+        // assetData();
+        fundsData(0x4e554445585f555344435f313800000000000000000000000000000000000000, 59902);
+        // accountData();
+        // updateTask();
 
         vm.stopBroadcast();
     }
@@ -81,7 +90,6 @@ contract MockData is Script {
     function assetData() public {
         // asset
         AssetParam memory assetParam = AssetParam(18, true, true, 1 ether, 1 ether, "Token_Alias");
-        // assetHandler.submitListAssetTask(TICKER, assetParam);
         assetHandler.listNewAsset(TICKER, assetParam);
         TokenInfo[] memory testTokenInfo = new TokenInfo[](1);
         testTokenInfo[0] = TokenInfo(CHAIN_ID, true, uint8(18), "0xContractAddress", "SYMBOL", 0);
@@ -93,21 +101,21 @@ contract MockData is Script {
         consolidateTaskParams[1] = ConsolidateTaskParam("fromAddr2", TICKER, CHAIN_ID, 2.5 ether);
         consolidateTaskParams[2] = ConsolidateTaskParam("fromAddr3", TICKER, CHAIN_ID, 3.3 ether);
         assetHandler.submitConsolidateTask(consolidateTaskParams);
-        assetHandler.consolidate("fromAddr1", TICKER, CHAIN_ID, 1 ether, "consolidate txHash1");
-        assetHandler.consolidate("fromAddr2", TICKER, CHAIN_ID, 2.5 ether, "consolidate txHash2");
-        assetHandler.consolidate("fromAddr3", TICKER, CHAIN_ID, 3.3 ether, "consolidate txHash3");
+        // assetHandler.consolidate("fromAddr1", TICKER, CHAIN_ID, 1 ether, "consolidate txHash1");
+        // assetHandler.consolidate("fromAddr2", TICKER, CHAIN_ID, 2.5 ether, "consolidate txHash2");
+        // assetHandler.consolidate("fromAddr3", TICKER, CHAIN_ID, 3.3 ether, "consolidate txHash3");
 
         assetHandler.tokenSwitch(TICKER, CHAIN_ID, false);
         assetHandler.tokenSwitch(TICKER, CHAIN_ID, true);
     }
 
-    function fundsData() public {
+    function fundsData(bytes32 _ticker, uint64 _chainId) public {
         // deposit
         DepositInfo[] memory depositInfos = new DepositInfo[](1);
         depositInfos[0] = DepositInfo(
             deployer,
-            CHAIN_ID,
-            TICKER,
+            _chainId,
+            _ticker,
             "124wd5urvxo4H3naXR6QACP1MGVpLeikeR",
             1 ether,
             "txHash",
@@ -115,29 +123,29 @@ contract MockData is Script {
             0
         );
         fundsHandler.submitDepositTask(depositInfos);
-        fundsHandler.recordDeposit(depositInfos[0]);
+        // fundsHandler.recordDeposit(depositInfos[0]);
 
         // withdraw
         WithdrawalInfo[] memory withdrawalInfos = new WithdrawalInfo[](1);
         withdrawalInfos[0] = WithdrawalInfo(
             deployer,
-            CHAIN_ID,
-            TICKER,
+            _chainId,
+            _ticker,
             "124wd5urvxo4H3naXR6QACP1MGVpLeikeR",
             1 ether
         );
 
         fundsHandler.submitWithdrawTask(withdrawalInfos);
-        fundsHandler.recordWithdrawal(
-            deployer,
-            CHAIN_ID,
-            TICKER,
-            "124wd5urvxo4H3naXR6QACP1MGVpLeikeR",
-            1 ether,
-            "TxHash"
-        );
-        fundsHandler.setPauseState(TICKER, false);
-        fundsHandler.setPauseState(bytes32(uint256(CHAIN_ID)), false);
+        // fundsHandler.recordWithdrawal(
+        //     deployer,
+        //     _chainId,
+        //     _ticker,
+        //     "124wd5urvxo4H3naXR6QACP1MGVpLeikeR",
+        //     1 ether,
+        //     "TxHash"
+        // );
+        // fundsHandler.setPauseState(_ticker, false);
+        // fundsHandler.setPauseState(bytes32(uint256(_chainId)), false);
     }
 
     function accountData() public {
